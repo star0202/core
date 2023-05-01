b345186b30ab26eb4dbfba6360272154f6199d37
Starcea <stardev.uwu@gmail.com>
2023-05-01 17:55:11 +0900

    feat: hover effect transition in LongButton

--- components/LongButton.tsx
+++ components/LongButton.tsx
@@ -5,21 +5,21 @@ const LongButton: React.FC<LongButtonProps> = ({ children, newTab=false, href, o
 	if(href) {
 		if(newTab)  return <a href={href} rel='noopener noreferrer'
 			target='_blank'>
-			<div className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1`}>
+			<div className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover transition duration-100 ease-in cursor-pointer px-4 py-4 mb-1`}>
 				{children}
 			</div>
 		</a>
 		else  return <Link href={href}>
-			<a className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1`}>
+			<a className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover transition duration-100 ease-in px-4 py-4 mb-1`}>
 				{children}
 			</a>
 		</Link>
 	}
-	if(onClick) return <div onKeyPress={onClick} onClick={onClick} className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1`}>
+	if(onClick) return <div onKeyPress={onClick} onClick={onClick} className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover transition duration-100 ease-in px-4 py-4 mb-1`}>
 		{children}
 	</div>
 
-	return <a  className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover cursor-pointer px-4 py-4 mb-1`}>
+	return <a  className={`${center ? 'justify-center ': '' }text-base bg-little-white dark:bg-discord-black text-black dark:text-gray-400 rounded flex hover:bg-little-white-hover dark:hover:bg-discord-dark-hover transition duration-100 ease-in px-4 py-4 mb-1`}>
 		{children}
 	</a>
 
